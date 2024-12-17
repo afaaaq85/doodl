4e3cd90667cd953e9374f0c5acd1e9684f8fd67a
Afaq Ahmed <afaaaq85@gmail.com>
2024-12-17 19:20:10 +0500

    fix require error for vercel deploy in tailwindcss config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate"
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -6,101 +8,101 @@ export default {
 		"./src/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			},
-    			'color-1': 'hsl(var(--color-1))',
-    			'color-2': 'hsl(var(--color-2))',
-    			'color-3': 'hsl(var(--color-3))',
-    			'color-4': 'hsl(var(--color-4))',
-    			'color-5': 'hsl(var(--color-5))'
-    		},
-    		boxShadow: {
-    			custom: '0px 4px 12px rgba(0, 0, 0, 0.1)',
-    			custom2: '0px 7px 29px 0px rgba(100, 100, 111, 0.2)'
-    		},
-    		animation: {
-    			'background-position-spin': 'background-position-spin 3000ms infinite alternate',
-    			meteor: 'meteor 5s linear infinite',
-    			rainbow: 'rainbow var(--speed, 2s) infinite linear'
-    		},
-    		keyframes: {
-    			'background-position-spin': {
-    				'0%': {
-    					backgroundPosition: 'top center'
-    				},
-    				'100%': {
-    					backgroundPosition: 'bottom center'
-    				}
-    			},
-    			meteor: {
-    				'0%': {
-    					transform: 'rotate(215deg) translateX(0)',
-    					opacity: '1'
-    				},
-    				'70%': {
-    					opacity: '1'
-    				},
-    				'100%': {
-    					transform: 'rotate(215deg) translateX(-500px)',
-    					opacity: '0'
-    				}
-    			},
-    			rainbow: {
-    				'0%': {
-    					'background-position': '0%'
-    				},
-    				'100%': {
-    					'background-position': '200%'
-    				}
-    			}
-    		}
-    	}
-    },
-	plugins: [require("tailwindcss-animate")],
+		extend: {
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			colors: {
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				chart: {
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				},
+				'color-1': 'hsl(var(--color-1))',
+				'color-2': 'hsl(var(--color-2))',
+				'color-3': 'hsl(var(--color-3))',
+				'color-4': 'hsl(var(--color-4))',
+				'color-5': 'hsl(var(--color-5))'
+			},
+			boxShadow: {
+				custom: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+				custom2: '0px 7px 29px 0px rgba(100, 100, 111, 0.2)'
+			},
+			animation: {
+				'background-position-spin': 'background-position-spin 3000ms infinite alternate',
+				meteor: 'meteor 5s linear infinite',
+				rainbow: 'rainbow var(--speed, 2s) infinite linear'
+			},
+			keyframes: {
+				'background-position-spin': {
+					'0%': {
+						backgroundPosition: 'top center'
+					},
+					'100%': {
+						backgroundPosition: 'bottom center'
+					}
+				},
+				meteor: {
+					'0%': {
+						transform: 'rotate(215deg) translateX(0)',
+						opacity: '1'
+					},
+					'70%': {
+						opacity: '1'
+					},
+					'100%': {
+						transform: 'rotate(215deg) translateX(-500px)',
+						opacity: '0'
+					}
+				},
+				rainbow: {
+					'0%': {
+						'background-position': '0%'
+					},
+					'100%': {
+						'background-position': '200%'
+					}
+				}
+			}
+		}
+	},
+	plugins: [tailwindcssAnimate],
 }
 
